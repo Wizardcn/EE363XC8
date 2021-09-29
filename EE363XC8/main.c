d512373bba69039e1d123b41b25dd3b211f5ba10
TONY <supawat125947@gmail.com>
2021-09-29 22:23:24 +0700

    code for Q5B

--- EE363XC8/main.c
+++ EE363XC8/main.c
@@ -1,51 +1,22 @@
-unsigned int ga, ub;
-
-void assign_number(void), assign_char(void), and(void), or(void), use_and_inc(void), use_and_dec(void), shift_right(void), shift_left(void);
-
+unsigned char ufunc(unsigned char a)
+{
+	unsigned char r = 0;
+	while (a)
+	{
+		r += 8;
+		a--;
+	}
+	return r;
+}
 
 int main(void)
 {
-	while(1){
-		assign_number();
-		assign_char();
-		and();
-		or();
-		use_and_inc();
-		use_and_dec();
-		shift_right();
-		shift_left();
-	}
+	ufunc(0);
+	ufunc(1);
+	ufunc(4);
+	ufunc(16);
+	ufunc(32);
+	ufunc(40);
+	ufunc(48);
+	ufunc(56);
 }
-
-void assign_number(void){
-	ga = 0x54;
-}
-
-void assign_char(void){
-	ub = 'K';
-}
-
-void and(void){
-	ga &= 0xF0;
-}
-
-void or(void){
-	ga |= 0b00001001;
-}
-
-void use_and_inc(void){
-	ga++;
-}
-
-void use_and_dec(void){
-	ub--;
-}
-
-void shift_right(void){
-	ga >>= 1;
-}
-
-void shift_left(void){
-	ub <<= 2;
-}
-
